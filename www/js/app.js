74c9dac8b07bec3d906ca55325055b800098ea60
florentplomb <plombf@gmail.com>
2015-03-13 11:39:34 +0100

    list in progress

--- www/js/app.js
+++ www/js/app.js
@@ -5,18 +5,43 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citizen-engagement.constants'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+.controller('MyCtrl', function($scope) {
+  
+  $scope.data = {
+    showDelete: false
+  };
+  
+  $scope.edit = function(item) {
+    alert('Edit Item: ' + item.id);
+  };
+  $scope.share = function(item) {
+    alert('Share Item: ' + item.id);
+  };
+  
+  $scope.moveItem = function(item, fromIndex, toIndex) {
+    $scope.items.splice(fromIndex, 1);
+    $scope.items.splice(toIndex, 0, item);
+  };
+  
+  $scope.onItemDelete = function(item) {
+    $scope.items.splice($scope.items.indexOf(item), 1);
+  };
+  
+  $scope.items = [
+    { id: 0 },
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+
+  ];
+  
 })
+  
+
 
 .run(function(AuthService, $rootScope, $state) {
 
@@ -109,3 +134,4 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
     $injector.get('$state').go('login'); // Go to the new issue tab by default.
   });
 });
+
