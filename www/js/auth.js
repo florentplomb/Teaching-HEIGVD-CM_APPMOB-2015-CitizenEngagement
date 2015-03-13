ec236dcdb5c8c69c0d650459216f9a6bf7298d37
florentplomb <plombf@gmail.com>
2015-03-13 09:41:18 +0100

    load skelete
    ets

---
+++ www/js/auth.js
@@ -0,0 +1,96 @@
+angular.module('citizen-engagement.auth', ['angular-storage'])
+
+  .service('AuthService', function(store) {
+
+    var service = {
+      currentUserId: store.get('currentUserId'),
+
+      setUser: function(user) {
+        service.currentUserId = user.userId;
+        store.set('currentUserId', user.userId);
+      },
+
+      unsetUser: function() {
+        service.currentUserId = null;
+        store.remove('currentUserId');
+      }
+    };
+
+    return service;
+  })
+
+  .factory('AuthInterceptor', function(AuthService) {
+    return {
+      request: function(config) {
+        if (AuthService.currentUserId) {
+          config.headers['X-User-Id'] = AuthService.currentUserId;
+        }
+
+        return config;
+      }
+    };
+  })
+
+  .config(function($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+  })
+
+  .controller('LoginCtrl', function(apiUrl, AuthService, $http, $ionicHistory, $ionicLoading, $scope, $state) {
+
+    // The $ionicView.beforeEnter event happens every time the screen is displayed.
+    $scope.$on('$ionicView.beforeEnter', function() {
+      // Initialize (or re-initialize) the user object.
+      // The first name and last name will be automatically filled from the form thanks to AngularJS's two-way binding.
+      $scope.user = {};
+    });
+
+    $scope.register = function() {
+
+      // Forget the previous error (if any).
+      delete $scope.error;
+
+      // Show a loading message if the request takes too long.
+      $ionicLoading.show({
+        template: 'Logging in...',
+        delay: 750
+      });
+
+      // Make the request to retrieve or create the user.
+      $http({
+        method: 'POST',
+        url: apiUrl + '/users/logister',
+        data: $scope.user
+      }).success(function(user) {
+
+        // If successful, give the user to the authentication service.
+        AuthService.setUser(user);
+
+        // Hide the loading message.
+        $ionicLoading.hide();
+
+        // Set the next view as the root of the history.
+        // Otherwise, the next screen will have a "back" arrow pointing back to the login screen.
+        $ionicHistory.nextViewOptions({
+          disableBack: true,
+          historyRoot: true
+        });
+
+        // Go to the issue creation tab.
+        $state.go('tab.newIssue');
+
+      }).error(function() {
+
+        // If an error occurs, hide the loading message and show an error message.
+        $ionicLoading.hide();
+        $scope.error = 'Could not log in.';
+      });
+    };
+  })
+
+  .controller('LogoutCtrl', function(AuthService, $scope, $state) {
+    $scope.logOut = function() {
+      AuthService.unsetUser();
+      $state.go('login');
+    };
+  })
+;
