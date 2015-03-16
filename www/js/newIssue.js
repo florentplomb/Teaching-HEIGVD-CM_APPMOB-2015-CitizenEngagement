b81c1ca2e68a6d39d86cbd6fefc4007fe95fe268
panchaudclelia <clelia.panchaud@gmail.com>
2015-03-16 08:39:10 +0100

    plugin camera

--- www/js/newIssue.js
+++ www/js/newIssue.js
@@ -13,6 +13,36 @@ angular.module('citizen-engagement.newIssue', [])
 
 	]
 
+function takePicture() {
+  navigator.camera.getPicture(function(imageURI) {
+
+    // imageURI is the URL of the image that we can use for
+    // an <img> element or backgroundImage.
+
+  }, function(err) {
+
+    // Ruh-roh, something bad happened
+
+  }, cameraOptions);
+}
+
+module.factory('Camera', ['$q', function($q) {
+
+  return {
+    getPicture: function(options) {
+      var q = $q.defer();
+
+      navigator.camera.getPicture(function(result) {
+        // Do any magic you need
+        q.resolve(result);
+      }, function(err) {
+        q.reject(err);
+      }, options);
+
+      return q.promise;
+    }
+  }
+}]);
 
 })
   
