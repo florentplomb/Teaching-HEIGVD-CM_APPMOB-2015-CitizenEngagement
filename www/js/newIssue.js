3345fbfe75a68a7bd10aca04c15b52d3c70cf86d
panchaudclelia <clelia.panchaud@gmail.com>
2015-03-16 13:32:55 +0100

    camera + map

--- www/js/newIssue.js
+++ www/js/newIssue.js
@@ -1,9 +1,12 @@
 angular.module('citizen-engagement.newIssue', [])
 
-.controller('NewIssueCtrl', function($scope) {
+.config(function($compileProvider){
+ $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+})
+
+  .controller('NewIssueCtrl', function($scope) {
 
 	$scope.types = [
-
 	{
 		desc: 'fire'
 	},
@@ -11,41 +14,44 @@ angular.module('citizen-engagement.newIssue', [])
 		desc : 'damage'
 	},
 
-	]
-
-function takePicture() {
-  navigator.camera.getPicture(function(imageURI) {
-
-    // imageURI is the URL of the image that we can use for
-    // an <img> element or backgroundImage.
-
-  }, function(err) {
-
-    // Ruh-roh, something bad happened
-
-  }, cameraOptions);
-}
-
-module.factory('Camera', ['$q', function($q) {
-
-  return {
-    getPicture: function(options) {
-      var q = $q.defer();
-
-      navigator.camera.getPicture(function(result) {
-        // Do any magic you need
-        q.resolve(result);
-      }, function(err) {
-        q.reject(err);
-      }, options);
-
-      return q.promise;
-    }
-  }
-}]);
-
+	];
 })
-  
+
+.factory('Camera', ['$q', function($q) {
+
+ return {
+   getPicture: function(options) {
+     var q = $q.defer();
+
+     navigator.camera.getPicture(function(result) {
+       // Do any magic you need
+       q.resolve(result);
+     }, function(err) {
+       q.reject(err);
+     }, options);
+
+     return q.promise;
+   }
+ }
+}])
+
+ .controller('photoCtrl', function($scope, Camera) {
+
+   $scope.getPhoto = function() {
+     Camera.getPicture().then(function(imageURI) {
+       console.log(imageURI);
+        $scope.lastPhoto = imageURI; //Last picture
+     }, function(err) {
+       console.err(err);
+     } , {
+     quality: 75,
+     targetWidth: 320,
+     targetHeight: 320,
+     saveToPhotoAlbum: false
+   }
+     )}
+   })
+
 
 
 
