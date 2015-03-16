b37476518ebc9f4f88638a9af08620abc2af0112
florentplomb <plombf@gmail.com>
2015-03-16 16:53:15 +0100

    get url param on details
    saint

--- www/js/issueDetails.js
+++ www/js/issueDetails.js
@@ -1,11 +1,15 @@
 
-var detailApp = angular.module('citizen-engagement.issueDetails',[])
+var detailApp = angular.module('citizen-engagement.issueDetails',[]);
 
 
-detailApp.controller = ('DetailCtrl',function($scope,$stateParams){
+detailApp.controller ('DetailCtrl',function($scope,$stateParams){
 
 
 console.log("ho"+$stateParams);
+
+var test = $stateParams;
+
+console.log(test);
 
 
 });
