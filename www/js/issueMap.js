4421ce56992f753982b2a54549a21d6e340173f3
panchaudclelia <clelia.panchaud@gmail.com>
2015-03-18 08:46:39 +0100

    geoloc OK

--- www/js/issueMap.js
+++ www/js/issueMap.js
@@ -1,39 +1,46 @@
 
-var mapApp = angular.module('citizen-engagement.issueMap', ["leaflet-directive"])
+	var mapApp = angular.module('citizen-engagement.issueMap', ["leaflet-directive", 'geolocation'])
 
 
 
-mapApp.controller("MapController", function($scope, mapboxMapId, mapboxAccessToken) {
+	mapApp.controller("MapController", function($log, $scope, mapboxMapId, mapboxAccessToken, geolocation) {
 
-var mapboxTileLayer = "http://api.tiles.mapbox.com/v4/" + "cleliapanchaud.kajpf86n";
-mapboxTileLayer = mapboxTileLayer + "/{z}/{x}/{y}.png?access_token=" + "pk.eyJ1IjoiY2xlbGlhcGFuY2hhdWQiLCJhIjoiM2hMOEVXYyJ9.olp7FrLzmzSadE07IY8OMQ";
-	$scope.mapDefaults = {
-	 tileLayer: mapboxTileLayer
-	 };
+	var mapboxTileLayer = "http://api.tiles.mapbox.com/v4/" + "cleliapanchaud.kajpf86n";
+	mapboxTileLayer = mapboxTileLayer + "/{z}/{x}/{y}.png?access_token=" + "pk.eyJ1IjoiY2xlbGlhcGFuY2hhdWQiLCJhIjoiM2hMOEVXYyJ9.olp7FrLzmzSadE07IY8OMQ";
+		$scope.mapDefaults = {
+		 tileLayer: mapboxTileLayer
+		 };
 
-	 $scope.mapCenter = {
-	 lat: 46.7833,
-	 lng: 6.65,
-	 zoom: 14
-	 };
+		$scope.mapCenter = {
+			lat: 46.7833,
+			lng: 6.65,
+			zoom: 14
+		};
 
- 	 
- 	 $scope.mapMarkers = [];
-	 //var issue = IssueService.getIssue();
-/* 	 $scope.mapMarkers.push({
-		lat: issue.lat,
-		lng: issue.lng,
- 		message: '<p>{{ issue.description }}</p><img src="{{ issue.imageUrl }}" width="200px" />',
- 			getMessageScope: function() {
-	 var scope = $scope.$new();
-	 //scope.issue = issue;
-	 return scope;
-	}
- 	});*/ 
-});
+		geolocation.getLocation().then(function(data) {
+			$scope.mapCenter.lat = data.coords.latitude;
+			$scope.mapCenter.lng = data.coords.longitude;
+		}, function(error) {
+			$log.error("Could not get location: " + error);
+		});
+
+	 	 
+	 	 $scope.mapMarkers = [];
+		 //var issue = IssueService.getIssue();
+	/* 	 $scope.mapMarkers.push({
+			lat: issue.lat,
+			lng: issue.lng,
+	 		message: '<p>{{ issue.description }}</p><img src="{{ issue.imageUrl }}" width="200px" />',
+	 			getMessageScope: function() {
+		 var scope = $scope.$new();
+		 //scope.issue = issue;
+		 return scope;
+		}
+	 	});*/ 
+	});
 
 
-  
+	  
 
 
 
--- www/js/newIssue.js
+++ www/js/newIssue.js
@@ -1,57 +1,55 @@
 angular.module('citizen-engagement.newIssue', [])
 
 .config(function($compileProvider){
- $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+	$compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
 })
 
-  .controller('NewIssueCtrl', function($scope) {
+.controller('NewIssueCtrl', function($scope) {
 
-	$scope.types = [
-	{
-		desc: 'fire'
-	},
-	{
-		desc : 'damage'
-	},
+	$scope.issue = {};
 
+	$scope.issueTypes = [
+		{ desc: 'fire' },
+		{ desc: 'damage' }
 	];
 })
 
 .factory('Camera', ['$q', function($q) {
 
- return {
-   getPicture: function(options) {
-     var q = $q.defer();
+	return {
+		getPicture: function(options) {
+			var q = $q.defer();
 
-     navigator.camera.getPicture(function(result) {
-       // Do any magic you need
-       q.resolve(result);
-     }, function(err) {
-       q.reject(err);
-     }, options);
+			navigator.camera.getPicture(function(result) {
+       		// Do any magic you need
+       			q.resolve(result);
+   			}, function(err) {
+   				q.reject(err);
+   			}, options);
 
-     return q.promise;
-   }
- }
+			return q.promise;
+		}
+	};
 }])
 
- .controller('photoCtrl', function($scope, Camera) {
+.controller('photoCtrl', function($scope, Camera) {
 
-   $scope.getPhoto = function() {
-     Camera.getPicture().then(function(imageURI) {
-       console.log(imageURI);
-        $scope.lastPhoto = imageURI; //Last picture
-     }, function(err) {
-       console.err(err);
-     } , {
-     quality: 75,
-     targetWidth: 320,
-     targetHeight: 320,
-     saveToPhotoAlbum: false
-   }
-     )}
-   })
+	$scope.getPhoto = function() {
+		Camera.getPicture({
+			quality: 75,
+			targetWidth: 320,
+			targetHeight: 320,
+			saveToPhotoAlbum: false
+		}).then(function(imageURI) {
+			console.log(imageURI);
+       		$scope.lastPhoto = imageURI; //Last picture
+    	}, function(err) {
+    		$scope.error = err;
+    		console.err(err);
+    	});
+    };
+})
 
 
 
-
+;
