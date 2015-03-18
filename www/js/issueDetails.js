8865259372d5b6cc02885331e5dd7b0652fc4028
florentplomb <plombf@gmail.com>
2015-03-18 13:14:26 +0100

    addnewcomment inprogress

--- www/js/issueDetails.js
+++ www/js/issueDetails.js
@@ -1,7 +1,15 @@
 
  var detailApp = angular.module('citizen-engagement.issueDetails',[])
 
+detailApp.controller('AddCommentCtrl',function($scope,IssuePostComment){
 
+$scope.addComment= function(){
+	
+	alert("hey");
+};
+        
+
+});
 
 
 detailApp.controller('DetailCtrl',function($scope,$stateParams,IssueByIdService){
@@ -11,7 +19,9 @@ var issueId = $stateParams.issueId;
         $scope.custom = true;
         $scope.toggleCustom = function() {
         $scope.custom = $scope.custom === false ? true: false;};
-        
+
+
+
 
 var callback = function(error, issues){
 	if (error) {
@@ -41,6 +51,21 @@ return {
 }
 });
 
+detailApp.factory("IssuePostComment", function($http, apiUrl) {
+return {	
+		postComment: function(callback,issueId,text){
+			 $http.post(apiUrl+"/issues/"+issueId+"/actions",{"type": "comment","payload": {"text": text}
+}).success(function(data){
+				issue = data;
+								
+				callback(null, issue);
+			}).error(function(error) {
+				callback(error);
+			});
+		}
+	
+}
+});
 
 
-1
+
